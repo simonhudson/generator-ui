61e1c46b16f0f9d498d099f12e3f3ca2537a27c3
Simon Hudson <hellosimonhudson+github@gmail.com>
2017-10-12 09:44:24 +0100

    base generator

---
+++ generators/app/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const __module = 'UI Generator';
+const Generator = require('yeoman-generator');
+
+module.exports = class extends Generator {
+
+	method1() {
+		this.log(1);
+	}
+
+	method2() {
+		this.log(2);
+	}
+
+};
