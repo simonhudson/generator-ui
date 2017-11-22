10fe4015f657203176aca52005a2c002a89b9b00
Simon Hudson <hellosimonhudson+github@gmail.com>
2017-11-22 11:29:46 +0000

    copy basic files

--- generators/app/index.js
+++ generators/app/index.js
@@ -3,24 +3,82 @@
 const __module = 'UI Generator';
 const Generator = require('yeoman-generator');
 const del = require('del');
-const mkDir = require('mkdirp');
+const mkdir = require('mkdirp');
 
-const paths = [
+const dirs = (self) => {
+	return {
+		template: {
+			root: `${self.templatePath()}\\`,
+			assets: `${self.templatePath()}\\assets\\`
+		},
+		destination: {
+			root: `${self.destinationPath()}\\`,
+			assets: `${self.destinationPath()}\\assets\\`
+		}
+	}
+};
 
-];
-
-const gulpTasks = [
-
-];
+const projectName = 'Acme project';
+const projectNameSlug = projectName.replace(/\s+/g, '-').toLowerCase();
 
 module.exports = class extends Generator {
 
-	method1() {
-		this.log(1);
+	_logActionStart(action) { return this.log(`\nSTART ${action}`); }
+	_logActionComplete(action) { return this.log(`COMPLETED ${action}`); }
+
+	greeting() {
+		this.log('********************');
+		this.log('*');
+		this.log(`* ${__module}`);
+		this.log('*');
+		this.log('********************');
 	}
 
-	method2() {
-		this.log(2);
+	clean() {
+		const action = 'Clean';
+		this._logActionStart(action);
+		del(this.destinationPath() + '/**/*', () => {
+			del(
+			[
+				self.destinationPath() + '/.yo-rc.json',
+				self.destinationPath() + '/.gitignore',
+				self.destinationPath() + '/.sass-cache',
+				self.destinationPath() + '/package.json',
+				self.destinationPath() + '/node_modules',
+			], () => {
+				self.config.save();
+				done();
+			});
+		});
+		this._logActionComplete(action);
+	}
+
+	writeFiles() {
+		const action = 'writeFiles';
+		const files = ['package.json', 'README.md'];
+		const config = {
+			projectName,
+			projectNameSlug
+		};
+		this._logActionStart(action);
+		files.forEach(file => {
+			this.fs.copyTpl(
+				`${dirs(this).template.root}${file}`,
+				`${dirs(this).destination.root}${file}`,
+				config
+			);
+		});
+		this._logActionComplete(action);
+	}
+
+	copyFiles() {
+		const action = 'copyFiles';
+		this._logActionStart(action);
+		this.fs.copy(
+			`${dirs(this).template.root}.gitignore`,
+			`${dirs(this).destination.root}.gitignore`
+		)
+		this._logActionComplete(action);
 	}
 
 };
