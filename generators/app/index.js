02bfd3f367ffa22e844222df6b4c33a2848edb11
Simon Hudson <hellosimonhudson+github@gmail.com>
2017-11-22 11:53:45 +0000

    Add boilerplate css

--- generators/app/index.js
+++ generators/app/index.js
@@ -21,10 +21,23 @@ const dirs = (self) => {
 const projectName = 'Acme project';
 const projectNameSlug = projectName.replace(/\s+/g, '-').toLowerCase();
 
+const npmUpdateDevDependencies = (self, pkg, version) => {
+	const packageContents = self.fs.readJSON(`${dirs(self).destination.root}package.json`);
+    packageContents.dependencies[pkg] = '*';
+    this.fs.writeJSON(`${dirs(self).destination.root}package.json`, packageContents);
+};
+
 module.exports = class extends Generator {
 
 	_logActionStart(action) { return this.log(`\nSTART ${action}`); }
 	_logActionComplete(action) { return this.log(`COMPLETED ${action}`); }
+
+	clean() {
+		const action = 'Clean';
+		this._logActionStart(action);
+		this.fs.delete(this.destinationPath() + '\\**\\*');
+		this._logActionComplete(action);
+	}
 
 	greeting() {
 		this.log('********************');
@@ -34,28 +47,9 @@ module.exports = class extends Generator {
 		this.log('********************');
 	}
 
-	clean() {
-		const action = 'Clean';
-		this._logActionStart(action);
-		del(this.destinationPath() + '/**/*', () => {
-			del(
-			[
-				self.destinationPath() + '/.yo-rc.json',
-				self.destinationPath() + '/.gitignore',
-				self.destinationPath() + '/.sass-cache',
-				self.destinationPath() + '/package.json',
-				self.destinationPath() + '/node_modules',
-			], () => {
-				self.config.save();
-				done();
-			});
-		});
-		this._logActionComplete(action);
-	}
-
 	writeFiles() {
 		const action = 'writeFiles';
-		const files = ['package.json', 'README.md'];
+		const files = ['package.json', 'README.md', 'index.html'];
 		const config = {
 			projectName,
 			projectNameSlug
@@ -77,6 +71,16 @@ module.exports = class extends Generator {
 		this.fs.copy(
 			`${dirs(this).template.root}.gitignore`,
 			`${dirs(this).destination.root}.gitignore`
+		)
+		this._logActionComplete(action);
+	}
+
+	copyAssets() {
+		const action = 'copyAssets';
+		this._logActionStart(action);
+		this.fs.copy(
+			`${dirs(this).template.assets}**\\*`,
+			`${dirs(this).destination.assets}`
 		)
 		this._logActionComplete(action);
 	}
