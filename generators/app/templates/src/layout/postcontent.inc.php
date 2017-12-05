bfa88439d7dcda2a095a0a85dd64a923eae1553c
Simon Hudson <hellosimonhudson+github@gmail.com>
2017-12-05 08:47:02 +0000

    Add wrap class

--- generators/app/templates/src/layout/postcontent.inc.php
+++ generators/app/templates/src/layout/postcontent.inc.php
@@ -1,4 +1,5 @@
-</main>
+		</div>
+	</main>
 
 <footer>
 </footer>
--- generators/app/templates/src/layout/precontent.inc.php
+++ generators/app/templates/src/layout/precontent.inc.php
@@ -22,15 +22,18 @@ include('functions/__import.inc.php');
     </nav>
 
     <header class="header">
-        <div class="site-logo">
-            <img alt="<?= $siteName; ?>" src="http://via.placeholder.com/175x75" />
+        <div class="wrap header__wrap">
+            <div class="site-logo">
+                <img alt="<?= $siteName; ?>" src="http://via.placeholder.com/175x75" />
+            </div>
+            <nav class="main-nav">
+                <button class="main-nav__toggle js-main-nav__toggle">Menu</button>
+                <ul class="main-nav__list js-main-nav__list">
+                    <?= mainNavList(); ?>
+                </ul>
+            </nav>
         </div>
-        <nav class="main-nav">
-            <button class="main-nav__toggle js-main-nav__toggle">Menu</button>
-            <ul class="main-nav__list js-main-nav__list">
-                <?= mainNavList(); ?>
-            </ul>
-        </nav>
     </header>
 
     <main class="main-content" id="main-content">
+        <div class="wrap">
